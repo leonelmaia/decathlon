37878d0ff1e4059b8e799b76537750f60b7784c9
Leonel Maia <l.maia@lafricamobile.com>
2025-08-20 16:22:28 +0200

    Dividing in two different directories

--- video-analysis/video_analysis.py
+++ video-analysis/video_analysis.py
@@ -1,7 +1,10 @@
 import argparse
 import logging
 import utils as deca
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", default="video.mp4")
@@ -15,6 +18,8 @@ if __name__ == "__main__":
     model = deca.load_model(args.model)
     logging.info(f"✅ Loaded model {args.model}")
 
-    unique_ids = deca.process_video(args.input, args.output, model, conf=args.conf, iou=args.iou)
+    unique_ids = deca.process_video(
+        args.input, args.output, model, conf=args.conf, iou=args.iou
+    )
     logging.info(f"✅ Saved annotated video to {args.output}")
     logging.info(f"👥 Total unique customers seen: {len(unique_ids)}")
